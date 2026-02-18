Created numerical evaluator for the mean <x> of the LogSkewNormal distribution, studied the behaviour at fixed mu=0.0, sigma=1.0 while varying the skewness parameter alpha. Started to work on a non-linear system solver to find the LogSkewNormal parameters (mean,sigma,alpha) when mean+-68% CI are given.

code/mbh_jaxns_asymm.py
@@ -85,9 +85,9 @@ if __name__ == "__main__":
         return jnp.where(in_bounds, log_like, -jnp.inf)
     
     def prior_model_logskewnormal():
-        a = yield Prior(tfpd.Uniform(-35.0, 35.0), name="a")
-        b = yield Prior(tfpd.Uniform(-20.0, 40.0), name="b")
-        true_sigma_gc = yield Prior(tfpd.Sample(tfpd.Uniform(0.0, 1.0e+3), sample_shape=(N_bh,)), name="true_sigma_gc")
+        a = yield Prior(tfpd.Uniform(-35.0, 35.0), name=r"$\beta$")
+        b = yield Prior(tfpd.Uniform(-20.0, 40.0), name=r"$\alpha$")
+        true_sigma_gc = yield Prior(tfpd.Sample(tfpd.Uniform(0.0, 1.0e+3), sample_shape=(N_bh,)), name=r"$\sigma_{gc,true}$")
         return a, b, true_sigma_gc
     
     model = Model(prior_model_logskewnormal, log_likelihood_logskewnormal)

code/test_logskewnorm.py
@@ -18,16 +18,16 @@ import pickle
 import tensorflow_probability.substrates.jax as tfp
 tfpd = tfp.distributions
 import matplotlib.pyplot as plt
-from utils import logskewnorm_logpdf, skewnorm_logpdf 
+from utils import logskewnormal_logpdf, skewnormal_logpdf, logskewnormal_pdf, logskewnormal_mean
 
 
-def test_logskewnorm_is_normalized():
+def test_logskewnormal_is_normalized():
     # Integrate over t = log(x), with x = exp(t), dx = exp(t) dt.
     # This avoids singular behavior near x=0 and improves numerical stability.
     def integrate_logpdf(loc, scale, shape, t_min=-50.0, t_max=50.0, n=200000):
         t = np.linspace(t_min, t_max, n)
         x = np.exp(t)
-        logf = np.asarray(logskewnorm_logpdf(x, loc=loc, scale=scale, shape=shape))
+        logf = np.asarray(logskewnormal_logpdf(x, loc=loc, scale=scale, shape=shape))
         y = np.exp(logf + t)
         return np.trapezoid(y, t)
 
@@ -51,15 +51,15 @@ if __name__ == "__main__":
     alpha = -6.0
 
     y_lognorm = sp.stats.lognorm.pdf(x, omega, loc=0.0, scale=jnp.exp(meanlog))
-    y_logskewnorm_noskew = jnp.exp(tfpd.Normal(meanlog, omega).log_prob(jnp.log(x))-jnp.log(x))
-    y_logskewnorm_noskew_custom = jnp.exp(logskewnorm_logpdf(x, loc=meanlog, scale=omega, shape=0.0))
-    y_logskewnorm_skew = jnp.exp(logskewnorm_logpdf(x, loc=meanlog, scale=omega, shape=alpha))
-    plt.figure("Lognormal vs Log-Skew-Normal PDF", figsize=(16, 12), dpi=300)
+    y_logskewnormal_noskew = jnp.exp(tfpd.Normal(meanlog, omega).log_prob(jnp.log(x))-jnp.log(x))
+    y_logskewnormal_noskew_custom = jnp.exp(logskewnormal_logpdf(x, mean=meanlog, sigma=omega, shape=0.0))
+    y_logskewnormal_skew = jnp.exp(logskewnormal_logpdf(x, mean=meanlog, sigma=omega, shape=alpha))
+    plt.figure("Lognormal vs Log-Skew-Normal PDF")
     plt.title("Lognormal vs Log-Skew-Normal PDF")
     plt.plot(x, y_lognorm, label="LogNormal")
-    plt.plot(x, y_logskewnorm_noskew, label=r"LogSkewNormal $(\alpha=0)$", linestyle="dashed")
-    plt.plot(x, y_logskewnorm_noskew_custom, label=r"LogSkewNormal $(\alpha=0)$ (custom)", linestyle="dotted")
-    plt.plot(x, y_logskewnorm_skew, label=r"LogSkewNormal $(\alpha=%.1lf)$ (custom)" % alpha)
+    plt.plot(x, y_logskewnormal_noskew, label=r"LogSkewNormal $(\alpha=0)$", linestyle="dashed")
+    plt.plot(x, y_logskewnormal_noskew_custom, label=r"LogSkewNormal $(\alpha=0)$ (custom)", linestyle="dotted")
+    plt.plot(x, y_logskewnormal_skew, label=r"LogSkewNormal $(\alpha=%.1lf)$ (custom)" % alpha)
     plt.legend(loc='best')
     plt.xscale("log")
     #plt.yscale("log")
@@ -68,21 +68,47 @@ if __name__ == "__main__":
 
     plt.figure(2)
     plt.title("Lognormal vs Log-Skew-Normal PDF")
-    plt.plot(x, jnp.abs(y_lognorm-y_logskewnorm_skew), label="LogNormal-LogSkewNormal")
+    plt.plot(x, jnp.abs(y_lognorm-y_logskewnormal_skew), label="LogNormal-LogSkewNormal")
     plt.legend(loc='best')
     plt.xscale("log")
     plt.xlabel("x")
 
-    x = jnp.linspace(-4.0, 4.0, 1000)
+    x = jnp.linspace(-6.0, 6.0, 1000)
     sigma = 1.0
     mu = 0.0
-    alpha = np.linspace(-2.0, 2.0, 5)
+    alpha = np.linspace(-10.0, 10.0, 5)
     plt.figure(3)
     plt.title("SkewNormal examples")
     for a in alpha:
-        plt.plot(x, jnp.exp(skewnorm_logpdf(x, mean=mu, sigma=sigma, shape=a)), label=r"SkewNormal $(\alpha=%.2lf)$" %a)
+        plt.plot(x, jnp.exp(skewnormal_logpdf(x, mean=mu, sigma=sigma, shape=a)), label=r"SkewNormal $(\alpha=%.2lf)$" %a)
     plt.legend(loc='best')
     plt.xlabel("x")
     plt.ylabel("p(x)")
-    plt.show()
 
+    x = jnp.logspace(-6.0, jnp.log10(20.0), 1000)
+    sigma = 1.0
+    mu = jnp.log(10.0)
+    alpha = np.linspace(-10.0, 10.0, 9)
+    plt.figure(4)
+    plt.title("SkewLogNormal examples")
+    plt.axvline(jnp.exp(mu+0.5*sigma**2), linestyle='dotted', color='black',label=r"Mean $(\alpha=%.2lf)$" %a)
+    for a in alpha:
+        plt.plot(x, logskewnormal_pdf(x, mean=mu, sigma=sigma, shape=a), label=r"LogSkewNormal $(\alpha=%.2lf)$" %a)
+        plt.axvline(logskewnormal_mean(mean=mu, sigma=sigma, shape=a)[0], linestyle='dashed', label=r"Mean $(\alpha=%.2lf)$" %a)
+    plt.legend(loc='best')
+    plt.xlabel("x")
+    #plt.xscale("log")
+    plt.ylabel("p(x)")
+
+    plt.figure(5)
+    plt.title(r"SkewLogNormal mean vs $\alpha$")
+    alphas = np.linspace(-20.0, 20.0, 1000)
+    means = jnp.array([logskewnormal_mean(mean=mu, sigma=sigma, shape=a)[0] for a in alphas])
+    plt.plot(alphas, means, label=r"Params: $\mu=%.1lf, \sigma=%.1lf$"%(mu,sigma))
+    plt.plot(alphas, jnp.abs(means-jnp.exp(mu+0.5*sigma**2)), label=r"Abs deviation from LogNormal mean")
+    plt.axhline(jnp.exp(mu+0.5*sigma**2), linestyle='dotted', color='black', label="LogNormal mean")
+    plt.legend(loc='best')
+    plt.xlabel(r"$\alpha$")
+    #plt.xscale("log")
+    plt.ylabel(r"$\left<x\right>$")
+    plt.show()